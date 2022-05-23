a16b4f7b6bb8d5e3395e190ecdb0f8f2ef412b14
Alex <terabyte95@mail.ru>
2022-05-23 18:46:38 +0500

    Some fix

--- CryptoTrackerV2/Services/DataManager.swift
+++ CryptoTrackerV2/Services/DataManager.swift
@@ -15,11 +15,11 @@ class DataManager {
     
     private init() {}
     
-    func setFavoriteStatus(for courseName: String, with status: Bool) {
-        userDefaults.set(status, forKey: courseName)
+    func setFavoriteStatus(for coinName: String, with status: Bool) {
+        userDefaults.set(status, forKey: coinName)
     }
     
-    func getFavoriteStatus(for courseName: String) -> Bool {
-        userDefaults.bool(forKey: courseName)
+    func getFavoriteStatus(for coinName: String) -> Bool {
+        userDefaults.bool(forKey: coinName)
     }
 }
